d732f67fad7c17476245f958dcb204f1d9c9f613
Henry Hay-Smith <hha100@uclive.ac.nz>
2021-04-21 21:09:07 +1200

    Minor changes

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,5 +1,6 @@
 import sys, socket, select
 
+#Custom errors for use with try/except
 class IncorrectConfigHeader(Exception):
     """Raised when the leftmost word of a line in the config file is not a recognised header"""
     pass
@@ -12,71 +13,83 @@ class PortDupeError(Exception):
     """Raised when the config file contains a duplicate port number"""
     pass
 
-prog_name = sys.argv[0]
-print()
+class EmptyConfigError(Exception):
+    """Raised when a line of the config file has a header but no value"""
+    pass
 
-if len(sys.argv) > 1:
-    config_filename = str(sys.argv[1])
+print() #To aid with readability of the text output in terminal
+
+if len(sys.argv) > 1:   #If there is a command line argument
+    config_filename = str(sys.argv[1])  #Save the name of the config file for this router to a variable
 else:
     print("Config filename not detected. Please use a command line argument of the form 'router1_config.txt'.\n")
-    sys.exit()
+    sys.exit()  #Exit the program with an error message if there is no command line argument
 
 try:
-    file = open(config_filename, 'r')
-    config_lines = file.readlines()
-    file.close()
+    file = open(config_filename, 'r')   #Try to open the file
+    config_lines = file.readlines()     #Save the lines from the config file in a list
+    file.close()                        #Close the file
 except:
-    print("File could not be found.\n")
-    sys.exit()
+    print("File could not be found or is not a readable .txt file.\n")
+    sys.exit()  #If the file can't be found and/or read, exit the program with an error message
 
 try:
     for index in range(len(config_lines)):
-        config_lines[index] = [x.strip('\n') for x in config_lines[index].split(' ')]
+        config_lines[index] = [x.strip('\n') for x in config_lines[index].split(' ')]   #Split each line into strings and strip any newline characters
 except:
-    print("Incorrect header detected on line {} of config file.\n".format(index))
-    sys.exit()
+    print("Incorrect line format on line {} of config file.\n".format(index))
+    sys.exit()  #Exit the program with an error message if there is a line in the config file that cannot be split into strings
 
-print(config_lines)
-
-router_id = -1
-input_ports = []
-output_ports = []
-timer = 180
+router_id = -1      #Set the router id to -1 initially so that it is possible to check if the router id has already been assigned to a correct value (between 1 and 64000)
+input_ports = []    #Create an empty list to hold the input ports of this router
+output_ports = []   #Create an empty list to hold the output ports of this router
+timer = 1010         #Set the default timer interval to _____ ~~~~~(find a good default value later)~~~~~
 
 try:
     for line in config_lines:
-        print("LINE IS:", line)
-        first = line[0]
-        if first == 'router-id':
-            print('1\n')
+        if len(line) < 2:
+            raise EmptyConfigError
+        
+        print("~~~~~~~~~ LINE IS:", line, "~~~~~~~~~")
+        first_str = line[0]
+        
+        #If the line being read is assigning the router ID
+        if first_str == 'router-id':
             
-            if router_id == -1 and line[1] in range(1, 64001):
-                router_id = line[0]
-                print("Router ID assigned correctly\n")
+            if router_id == -1 and int(line[1]) in range(1, 64001):
+                router_id = line[1]
+                print("Router ID assigned to {}\n".format(router_id))
             else:
                 raise RouterIDError
             
-        elif first == 'input-ports':
-            print('2\n')
-            if line[1] in input_ports or line[1] in output_ports:
-                raise PortError
+        #If the line being read is assigning the input ports
+        elif first_str == 'input-ports':
+            for index in range(1, len(line)):
+                if line[index] in input_ports or line[index] in output_ports:
+                    raise PortError
             
-            elif type(line[1]) == type(0) and line[1] in range(1024, 64001):
-                pass
+                elif line[index].isnumeric() and int(line[index]) in range(1024, 64001):
+                    input_ports.append(line[index])
             
-        elif first == 'output-ports':
-            print('3\n')
-            
-        elif first == 'timer':
-            #if line[1] == int(line[0]) and int(line[0]) in range()
+            print('Input ports assigned to {}\n'.format([x for x in input_ports]))
+        
+        #If the line being read is assigning the output ports
+        elif first_str == 'output-ports':
+            for index in range(1, len(line)):
+                output_port = [x.strip('\n') for x in line[index].split('-')]
+                print("-=-=-=-=-=-=-=- Port is:", output_port, "-=-=-=-=-=-=-=-")
+            print('Output ports assigned to {}\n'.format([x for x in output_ports]))
+        
+        #If the line being read is assigning the timer interval
+        elif first_str == 'timer':
             timer = line[1]
-            print('4\n')
+            print('TIMER IS: {}\n'.format(timer))    #FOR DEBUGGING
             
         else:
             raise IncorrectConfigHeader
         
 except IncorrectConfigHeader:
-    print("One of the headers in the config file cannot be processed. Make sure that the header is one of the following:\nrouter-id, input-port, output-port, timer\n")
+    print("One of the headers in the config file cannot be processed. Make sure that the header is one of the following:\nrouter-id, input-ports, output-ports, timer\n")
     sys.exit()
     
 except RouterIDError:
@@ -97,6 +110,6 @@ except:
 
 #print("\nIncorrect header detected on line {} of config file.\n".format(Exception))
 
-while True:
-    #use select() to block until events occur
-    pass
+#while True:
+    ##use select() to block until events occur
+    #pass
