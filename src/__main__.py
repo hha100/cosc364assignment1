e4d36a9086f2cb5a96c2fc62ec366e8588469a44
Ryan Akers <rak80@uclive.ac.nz>
2021-04-24 18:17:49 +1200

    Further refactoring of file layout to run parser from main
    Also automatically added spaces between all # and comments because apparently it's better style? According to my IDE

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,13 @@
 """__main__.py is run by default when naming the directory in the command line, meaning you don't need to specify
 which file you want to run
-e.g. "py src conf/config1.txt" """
+e.g. 'py src conf/config1.txt' """
 
 import routingtable
 import configparser
+import sys
 
 
 print("hi")
+
+config_filename = str(sys.argv[1])
+configparser.parse(config_filename)
